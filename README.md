f017ad68b6e0053e3f64e070282191bbe9ac5146
Søren Lund <s.lund@consult.nordea.com>
2016-03-08 09:41:21 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple demonstration of how to skip plugin executions in a Apache Maven build.
---
+++ src/main/java/net/twonky/demo/App.java
@@ -0,0 +1,13 @@
+package net.twonky.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
